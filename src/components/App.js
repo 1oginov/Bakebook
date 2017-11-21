5f28f5ed7273d8423e6a596d80a774a4cbe40612
Danila Loginov <danila@loginov.rocks>
2017-11-21 15:10:51 +0300

    feat: Login component and authentication with Firebase added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { getTheme } from 'react-native-material-kit';
+import firebase from 'firebase';
 
 import Loader from './Loader';
+import Login from './Login';
 
 const theme = getTheme();
 
@@ -20,11 +22,49 @@ const styles = StyleSheet.create({
   },
 });
 
-const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.headline}>Bakebook</Text>
-    <Loader />
-  </View>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: null,
+    };
+  }
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyADc229G7MWz52sJ0V_KKMJWoIywjTcEfQ',
+      authDomain: 'loginov-rocks-bakebook.firebaseapp.com',
+      databaseURL: 'https://loginov-rocks-bakebook.firebaseio.com',
+      projectId: 'loginov-rocks-bakebook',
+      storageBucket: 'loginov-rocks-bakebook.appspot.com',
+      messagingSenderId: '576336141494',
+    });
+
+    firebase.auth().onAuthStateChanged(user => this.setState({ isAuthenticated: !!user }));
+  }
+
+  renderInitialView() {
+    switch (this.state.isAuthenticated) {
+      case true:
+        return <Text>Authenticated</Text>;
+
+      case false:
+        return <Login />;
+
+      default:
+        return <Loader />;
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headline}>Bakebook</Text>
+        {this.renderInitialView()}
+      </View>
+    );
+  }
+}
 
 export default App;
