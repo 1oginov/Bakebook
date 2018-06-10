4b42059dc418016f8396d5c6413038de75e55b04
Danila Loginov <danila@loginov.rocks>
2018-06-11 02:42:45 +0300

    feat: implement `Profile` view with logout button, update Navigator

--- src/components/Navigator/Navigator.js
+++ src/components/Navigator/Navigator.js
@@ -1,13 +1,15 @@
-import { TabNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation';
 
+import Profile from '../../user/views/Profile';
 import CategoryList from '../../vehicles/components/CategoryList';
 import CreateVehicle from '../../vehicles/components/CreateVehicle';
 import VehicleList from '../../vehicles/components/VehicleList';
 
-const Navigator = TabNavigator({
-  VehicleList: { screen: VehicleList },
-  CreateVehicle: { screen: CreateVehicle },
-  CategoryList: { screen: CategoryList },
+const Navigator = createBottomTabNavigator({
+  Profile,
+  VehicleList,
+  CreateVehicle,
+  CategoryList,
 });
 
 export default Navigator;
